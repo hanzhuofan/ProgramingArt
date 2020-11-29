75f11e1a47124cabbcf8b8c02b26b604a3c5c9c5
hanzhuofan <310756323@qq.com>
2020-11-29 11:50:36 +0800

    version 2.4.0

---
+++ src/main/java/com/hzf/study/program/SpringCode.java
@@ -0,0 +1,13 @@
+package com.hzf.study.program;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author hanzhuofan
+ * @date 2020/11/14 18:26
+ */
+public class SpringCode {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+    }
+}
