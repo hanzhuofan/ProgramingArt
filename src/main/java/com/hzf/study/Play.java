96b41e43cff89d217026c9554413ad2b94009853
zhuofan.han(韩卓帆) <zhuofan.han@advantech.com.cn>
2021-04-13 09:07:01 +0800

    add oshi

--- src/main/java/com/hzf/study/Play.java
+++ src/main/java/com/hzf/study/Play.java
@@ -3,20 +3,32 @@ package com.hzf.study;
 import com.hzf.study.controller.GreetingController;
 import com.hzf.study.service.GreetingService;
 import com.hzf.study.utils.DateHelper;
+import com.hzf.study.utils.RuntimeUtils;
+import com.sun.management.OperatingSystemMXBean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
+import oshi.util.Util;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.lang.reflect.Method;
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @Author zhuofan.han
  * @Date 2020/11/11 17:14
  */
+@Slf4j
 public class Play {
     public static void main(String[] args) throws Exception {
         GreetingController greetingController = new GreetingController();
@@ -63,6 +75,8 @@ public class Play {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
 
+        System.out.println(DateFormat.getInstance().format(DateHelper.getEndOfDay(now)));
+        System.out.println(DateFormat.getInstance().format(DateHelper.getEndOfDay(DateHelper.getStartOfDay(now) - 1000)));
         System.out.println(DateFormat.getInstance().format(DateHelper.getStartOfWeek(DateHelper.getLastWeek(now))));
         System.out.println(DateFormat.getInstance().format(DateHelper.getStartOfWeek(now)));
         System.out.println(DateFormat.getInstance().format(DateHelper.getStartOfWeek(DateHelper.getNextWeek(now))));
@@ -75,6 +89,96 @@ public class Play {
         set.remove(new Test(1, 3));
         set.add(new Test(1, 3));
         System.out.println(set);
+
+        Boolean a = true;
+        Boolean b = true;
+        Boolean c = false;
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+
+        System.out.println("=============================");
+        System.out.println(RuntimeUtils.getSystemCpuLoad());
+        System.out.println(RuntimeUtils.getProcessCpuLoad());
+        System.out.println(RuntimeUtils.getTotalPhysicalMemorySize());
+        System.out.println(RuntimeUtils.getFreePhysicalMemorySize());
+        System.out.println(RuntimeUtils.getUsedPhysicalMemorySize());
+
+//        for (int i = 0; i < 4; i++) {
+//            new Thread(() -> {
+//                while (true) {
+//                    long bac = 1000000;
+//                    bac = bac >> 1;
+//                }
+//            }).start();
+//        }
+//
+//        while(true) {
+//            SystemInfo systemInfo = new SystemInfo();
+////            GlobalMemory memory = systemInfo.getHardware().getMemory();
+////            long totalByte = memory.getTotal();
+////            long acaliableByte = memory.getAvailable();
+////
+////            log.info("内存大小 = {},内存使用率 ={}",formatByte(totalByte),new DecimalFormat("#.##%").format((totalByte-acaliableByte)*1.0/totalByte));
+//
+//            CentralProcessor processor = systemInfo.getHardware().getProcessor();
+//            long[] prevTicks = processor.getSystemCpuLoadTicks();
+//            long[][] processorCpuLoadTicks = processor.getProcessorCpuLoadTicks();
+//            // Wait a second...
+//            Util.sleep(1000);
+//            System.out.println("cpu当前使用率1==:" + new DecimalFormat("#.##%").format(processor.getSystemCpuLoadBetweenTicks(prevTicks)));
+//            System.out.println("cpu当前使用率2==:" + new DecimalFormat("#.##%").format(osmxb.getSystemCpuLoad()));
+//            System.out.println("cpu当前使用率3==:" + new DecimalFormat("#.##%").format(Play.getInstance().getProcessCpu()));
+//            Thread.sleep(0);
+//        }
+    }
+
+    private static OperatingSystemMXBean osmxb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+    private static Play instance = new Play();
+
+    private java.lang.management.OperatingSystemMXBean osMxBean;
+    private ThreadMXBean threadBean;
+    private long preTime = System.nanoTime();
+    private long preUsedTime = 0;
+
+    private Play() {
+        osMxBean = ManagementFactory.getOperatingSystemMXBean();
+        threadBean = ManagementFactory.getThreadMXBean();
+    }
+
+    public static Play getInstance() {
+        return instance;
+    }
+
+    public double getProcessCpu() {
+        long totalTime = 0;
+        for (long id : threadBean.getAllThreadIds()) {
+            totalTime += threadBean.getThreadCpuTime(id);
+        }
+        long curtime = System.nanoTime();
+        long usedTime = totalTime - preUsedTime;
+        long totalPassedTime = curtime - preTime;
+        preTime = curtime;
+        preUsedTime = totalTime;
+        return (((double) usedTime) / totalPassedTime / osMxBean.getAvailableProcessors());
+    }
+
+    public static String formatByte(long byteNumber){
+        double FORMAT = 1024.0;
+        double kbNumber = byteNumber/FORMAT;
+        if(kbNumber<FORMAT){
+            return new DecimalFormat("#.##KB").format(kbNumber);
+        }
+        double mbNumber = kbNumber/FORMAT;
+        if(mbNumber<FORMAT){
+            return new DecimalFormat("#.##MB").format(mbNumber);
+        }
+        double gbNumber = mbNumber/FORMAT;
+        if(gbNumber<FORMAT){
+            return new DecimalFormat("#.##GB").format(gbNumber);
+        }
+        double tbNumber = gbNumber/FORMAT;
+        return new DecimalFormat("#.##TB").format(tbNumber);
     }
 
     @Data
@@ -85,8 +189,12 @@ public class Play {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             Test test = (Test) o;
             return a == test.a;
         }
---
+++ src/main/java/com/hzf/study/utils/RuntimeUtils.java
@@ -0,0 +1,133 @@
+package com.hzf.study.utils;
+
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
+
+/**
+ * 系统环境工具类
+ *
+ * @author zhuofan.han
+ * @date 2021/4/9 17:06
+ */
+public class RuntimeUtils {
+    private static final OperatingSystemMXBean systemMXBean = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
+    private static final Runtime runtime = Runtime.getRuntime();
+
+    /**
+     * 获取物理内存总大小
+     *
+     * @return
+     */
+    public static long getTotalPhysicalMemorySize() {
+        return systemMXBean.getTotalPhysicalMemorySize();
+    }
+
+    /**
+     * 获取物理内存剩余大小
+     *
+     * @return
+     */
+    public static long getFreePhysicalMemorySize() {
+        return systemMXBean.getFreePhysicalMemorySize();
+    }
+
+    /**
+     * 获取物理内存已使用大小
+     *
+     * @return
+     */
+    public static long getUsedPhysicalMemorySize() {
+        return systemMXBean.getTotalPhysicalMemorySize() - systemMXBean.getFreePhysicalMemorySize();
+    }
+
+    /**
+     * 获取 Swap 总大小
+     *
+     * @return
+     */
+    public static long getTotalSwapSpaceSize() {
+        return systemMXBean.getTotalSwapSpaceSize();
+    }
+
+    /**
+     * 获取 Swap 剩余大小
+     *
+     * @return
+     */
+    public static long getFreeSwapSpaceSize() {
+        return systemMXBean.getFreeSwapSpaceSize();
+    }
+
+    /**
+     * 获取 Swap 已使用大小
+     *
+     * @return
+     */
+    public static long getUsedSwapSpaceSize() {
+        return systemMXBean.getTotalSwapSpaceSize() - systemMXBean.getFreeSwapSpaceSize();
+    }
+
+    /**
+     * 获取 JVM 最大内存
+     *
+     * @return
+     */
+    public static long getJvmMaxMemory() {
+        return runtime.maxMemory();
+    }
+
+    /**
+     * 获取 JVM 内存总大小
+     *
+     * @return
+     */
+    public static long getJvmTotalMemory() {
+        return runtime.totalMemory();
+    }
+
+    /**
+     * 获取 JVM 内存剩余大小
+     *
+     * @return
+     */
+    public static long getJvmFreeMemory() {
+        return runtime.freeMemory();
+    }
+
+    /**
+     * 获取 JVM 内存已使用大小
+     *
+     * @return
+     */
+    public static long getJvmUsedMemory() {
+        return runtime.totalMemory() - runtime.freeMemory();
+    }
+
+    /**
+     * 获取 JVM 内存 使用率
+     *
+     * @return
+     */
+    public static long getJvmUsedMemoryLoad() {
+        return getJvmUsedMemory() * 100 / getJvmTotalMemory();
+    }
+
+    /**
+     * 获取系统 CPU 使用率
+     *
+     * @return
+     */
+    public static double getSystemCpuLoad() {
+        return systemMXBean.getSystemCpuLoad();
+    }
+
+    /**
+     * 获取 JVM 进程 CPU 使用率
+     *
+     * @return
+     */
+    public static double getProcessCpuLoad() {
+        return systemMXBean.getProcessCpuLoad();
+    }
+}
