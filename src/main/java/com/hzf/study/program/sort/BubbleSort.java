6827341557d122595bc855018173752c5c0da136
zhuofan.han(韩卓帆) <zhuofan.han@advantech.com.cn>
2021-06-23 14:00:31 +0800

    Add study

---
+++ src/main/java/com/hzf/study/program/sort/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.hzf.study.program.sort;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * @author zhuofan.han
+ * @date 2021/6/23
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+        sort(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
