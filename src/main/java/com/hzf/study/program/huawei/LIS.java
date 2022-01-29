3137e6585e6a373f968bd223a0c12e58bfa3d683
hanzhuofan <310756323@qq.com>
2022-01-29 22:25:20 +0800

    add code

--- src/main/java/com/hzf/study/program/huawei/LIS.java
+++ src/main/java/com/hzf/study/program/huawei/LIS.java
@@ -11,36 +11,49 @@ public class LIS {
     public static void main(String[] args) {
         LIS lis = new LIS();
         System.out.println(Arrays.toString(lis.LIS(new int[] {2, 1, 5, 3, 6, 4, 8, 9, 7})));
+        System.out.println(Arrays.toString(lis.LIS(new int[] {1, 2, 8, 6, 4})));
     }
 
     /**
      * retrun the longest increasing subsequence
      *
-     * @param arr int整型一维数组 the array
+     * @param arr
+     *            int整型一维数组 the array
      * @return int整型一维数组
      */
     public int[] LIS(int[] arr) {
         // write code here
+        int len = 1;
+        // end用来记录长度所对应的最大值,最长就是 n，加速匹配dp
+        int[] end = new int[arr.length + 1];
+        end[len] = arr[0];
+        // 记录i位置的最大递增长度
         int[] dp = new int[arr.length];
-        Arrays.fill(dp, 1);
-        int max = 0;
+        dp[0] = len;
         for (int i = 1; i < arr.length; i++) {
-            int j = i - 1;
-            while (j >= 0 && arr[j] > arr[i]) {
-                j--;
-            }
-            if (j > 0) {
-                dp[i] = dp[j] + 1;
-                if (dp[i] > max) {
-                    max = dp[i];
+            if (arr[i] > end[len]) {
+                end[++len] = arr[i];
+                dp[i] = len;
+            } else {
+                // 如果不是 那就从 1到 之前递增间找一个递增的
+                int left = 1, right = len, pos = 0;
+                while (left <= right) {
+                    int mid = (left + right) >> 1;
+                    if (end[mid] < arr[i]) {
+                        pos = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
                 }
+                end[pos + 1] = arr[i];
+                dp[i] = pos + 1;
             }
         }
-        int[] ans = new int[max];
-        for (int i = arr.length - 1; i >= 0;i--) {
-            int index = dp[i] - 1;
-            if (ans[index] > arr[i]) {
-                ans[index] = arr[i];
+        int[] ans = new int[len];
+        for (int i = arr.length - 1; len > 0; i--) {
+            if (dp[i] == len) {
+                ans[--len] = arr[i];
             }
         }
         return ans;
